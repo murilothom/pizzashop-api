6055dea0a7925e1943a73ca299534f9305a0339d
Murilo Thom <murilothom.2002@hotmail.com>
2024-05-22 23:00:18 -0300

    schema para table auth_link

--- drizzle/0000_rich_tag.sql
+++ drizzle/0000_rich_tag.sql
@@ -18,15 +18,28 @@ CREATE TABLE IF NOT EXISTS "users" (
 CREATE TABLE IF NOT EXISTS "restaurants" (
 	"id" text PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
-	"description" text NOT NULL,
+	"description" text,
 	"manager_id" text,
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "restaurants_description_unique" UNIQUE("description")
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "auth_links" (
+	"id" text PRIMARY KEY NOT NULL,
+	"code" text NOT NULL,
+	"user_id" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "auth_links_code_unique" UNIQUE("code")
 );
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "restaurants" ADD CONSTRAINT "restaurants_manager_id_users_id_fk" FOREIGN KEY ("manager_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "auth_links" ADD CONSTRAINT "auth_links_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
--- drizzle/0001_rich_randall.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "restaurants" DROP CONSTRAINT "restaurants_description_unique";--> statement-breakpoint
-ALTER TABLE "restaurants" ALTER COLUMN "description" DROP NOT NULL;
