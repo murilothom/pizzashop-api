212bac1f670a62762d36d3acf85b34cd5bfefc7c
Murilo Thom <murilothom.2002@hotmail.com>
2024-05-22 21:31:29 -0300

    corrigido schema da table users e restaurants e criado seeds

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'drizzle-kit';
+import { env } from './src/env';
 
 export default {
   dialect: 'postgresql',
   schema: './src/db/schema/index.ts',
-  out: './drizzle'
+  out: './drizzle',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
 } satisfies Config;
---
+++ src/db/seed.ts
@@ -0,0 +1,67 @@
+import { faker } from '@faker-js/faker';
+import { users, restaurants } from './schema';
+import { db } from './connection';
+
+/**
+ * Reset database
+ */
+await db.delete(users);
+await db.delete(restaurants);
+
+console.log('Database reset ✅');
+
+/**
+ * Create customers
+ */
+
+await db.insert(users).values([
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: 'customer',
+  },
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: 'customer',
+  },
+]);
+
+console.log('Customers created ✅');
+
+/**
+ * Create manager
+ */
+
+const [manager] = await db
+  .insert(users)
+  .values([
+    {
+      name: faker.person.fullName(),
+      email: 'admin@admin.com',
+      role: 'manager',
+    },
+  ])
+  .returning({
+    id: users.id,
+  });
+
+console.log('Manager created ✅');
+
+/**
+ * Create restaurant
+ */
+
+await db.insert(restaurants).values([
+  {
+    name: faker.company.name(),
+    description: faker.lorem.paragraph(),
+    managerId: manager.id,
+  },
+]);
+
+console.log('Restaurant created ✅');
+
+console.log('🐱‍🏍Database seeded successfully🐱‍💻');
+
+process.exit();
