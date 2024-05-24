00ac7e26cec873e03e24f5aec9add255bd9ebf00
Murilo Thom <murilothom.2002@hotmail.com>
2024-05-24 08:46:06 -0300

    seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,40 +1,53 @@
 /* eslint-disable drizzle/enforce-delete-with-where */
 
 import { faker } from '@faker-js/faker'
-import { users, restaurants } from './schema'
+import {
+  users,
+  restaurants,
+  orderItems,
+  products,
+  orders,
+  authLinks,
+} from './schema'
 import { db } from './connection'
+import { createId } from '@paralleldrive/cuid2'
 
 /**
  * Reset database
  */
 await db.delete(users)
 await db.delete(restaurants)
+await db.delete(orderItems)
+await db.delete(orders)
+await db.delete(products)
+await db.delete(authLinks)
 
 console.log('Database reset ✅')
 
 /**
  * Create customers
  */
-
-await db.insert(users).values([
-  {
-    name: faker.person.fullName(),
-    email: faker.internet.email(),
-    role: 'customer',
-  },
-  {
-    name: faker.person.fullName(),
-    email: faker.internet.email(),
-    role: 'customer',
-  },
-])
+const [firstCustomer, secondCustomer] = await db
+  .insert(users)
+  .values([
+    {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      role: 'customer',
+    },
+    {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      role: 'customer',
+    },
+  ])
+  .returning()
 
 console.log('Customers created ✅')
 
 /**
  * Create manager
  */
-
 const [manager] = await db
   .insert(users)
   .values([
@@ -53,17 +66,101 @@ console.log('Manager created ✅')
 /**
  * Create restaurant
  */
-
-await db.insert(restaurants).values([
-  {
-    name: faker.company.name(),
-    description: faker.lorem.paragraph(),
-    managerId: manager.id,
-  },
-])
+const [restaurant] = await db
+  .insert(restaurants)
+  .values([
+    {
+      name: faker.company.name(),
+      description: faker.lorem.paragraph(),
+      managerId: manager.id,
+    },
+  ])
+  .returning()
 
 console.log('Restaurant created ✅')
 
-console.log('🐱‍🏍Database seeded successfully🐱‍💻')
+function generateProducts() {
+  return {
+    name: faker.commerce.productName(),
+    restaurantId: restaurant.id,
+    description: faker.commerce.productDescription(),
+    priceInCents: Number(faker.commerce.price({ min: 190, max: 490, dec: 0 })),
+  }
+}
+
+/**
+ * Create products
+ */
+const availableProducts = await db
+  .insert(products)
+  .values([
+    generateProducts(),
+    generateProducts(),
+    generateProducts(),
+    generateProducts(),
+    generateProducts(),
+    generateProducts(),
+  ])
+  .returning()
+
+console.log('Products created ✅')
+
+/**
+ * Create orders and order items
+ */
+type OrderItemInsert = typeof orderItems.$inferInsert
+type OrdersInsert = typeof orders.$inferInsert
+
+const orderItemsToInsert: OrderItemInsert[] = []
+const ordersToInsert: OrdersInsert[] = []
+
+for (let i = 0; i < 200; i++) {
+  const orderId = createId()
+
+  const orderProducts = faker.helpers.arrayElements(availableProducts, {
+    min: 1,
+    max: 3,
+  })
+
+  let totalInCents = 0
+
+  orderProducts.forEach((orderProduct) => {
+    const quantity = faker.number.int({ min: 1, max: 3 })
+
+    totalInCents += orderProduct.priceInCents * quantity
+
+    orderItemsToInsert.push({
+      orderId,
+      priceInCents: orderProduct.priceInCents,
+      quantity,
+      productId: orderProduct.id,
+    })
+  })
+
+  ordersToInsert.push({
+    id: orderId,
+    customerId: faker.helpers.arrayElement([firstCustomer, secondCustomer]).id,
+    restaurantId: restaurant.id,
+    totalInCents,
+    status: faker.helpers.arrayElement([
+      'pending',
+      'processing',
+      'delivering',
+      'delivered',
+      'canceled',
+    ]),
+    createdAt: faker.date.recent({ days: 40 }),
+  })
+}
+
+await db.insert(orders).values(ordersToInsert)
+
+console.log('Orders created ✅')
+
+await db.insert(orderItems).values(orderItemsToInsert)
+
+console.log('OrderItems created ✅')
+
+console.log('🐱‍🏍Database seeded successfully🐱‍🏍')
 
 process.exit()
